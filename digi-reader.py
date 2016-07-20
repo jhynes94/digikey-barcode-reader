2727021a64ed8f89bb492c60cc8c2c3686ebaecb
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2016-07-20 14:00:06 -0400

    First Commit

--- digi-reader.py
+++ digi-reader.py
@@ -24,11 +24,26 @@ if __name__ == "__main__":
 
 		d = None
 		# parse barcode 1786439000000040970571
-		sys.stderr.write('info: code readed: {}\n'.format(line))
+		sys.stderr.write('info: code read: {}\n'.format(line))
 
 		if len(line) == 23: # digikey bag code
 			prod_id = line[0:7]
 			qnt = line[8:16]
+
+			#If the code ends in a zero (Remove it due to search engine problems)
+			if line[6] == "0":
+				newList = list(prod_id)
+				del(newList[6])
+				prod_id = "".join(newList)
+				sys.stderr.write('info: String end popped: {}\n'.format(prod_id))
+
+			#If the code starts with a zero (Remove it due to search engine problems)
+			if line[0] == "0":
+				newList = list(prod_id)
+				del(newList[0])
+				prod_id = "".join(newList)
+				sys.stderr.write('info: String front popped: {}\n'.format(prod_id))
+
 
 			d = digikey2data(id2digi_pn(prod_id))
 		elif '-ND' in line: # digikey part/number
